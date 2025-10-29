50b147a81c609a45bac96e675b53928051d6ce2e
djgigabyte <73799758+djgigabyte@users.noreply.github.com>
2025-10-28 17:01:33 -0700

    Removing uneeded white space and comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Teleop.java
@@ -9,7 +9,6 @@ import org.firstinspires.ftc.teamcode.robot.visionTest;
 @TeleOp(name = "Teleop", group = "Linear OpMode")
 public class Teleop extends LinearOpMode {
     static double SLOWMO_POWER_SCALE = .2;
-
     public void runOpMode() {
         DcMotor leftFront = hardwareMap.get(DcMotor.class, "leftFront");
         DcMotor leftBack = hardwareMap.get(DcMotor.class, "leftBack");
@@ -26,18 +25,16 @@ public class Teleop extends LinearOpMode {
 
         while (opModeIsActive()) {
             // Translation and rotation controls
-
             {
                 double motorPowerScaling = gamepad1.left_bumper ? SLOWMO_POWER_SCALE : 1;
                 double x = gamepad1.left_stick_x;
-                double y = -gamepad1.left_stick_y; // Invert y to match coordinate system
+                double y = -gamepad1.left_stick_y;
                 double turn = gamepad1.right_stick_x;
                 leftFront.setPower((x + y + turn) * motorPowerScaling);
                 leftBack.setPower((-x + y + turn) * motorPowerScaling);
                 rightBack.setPower((x + y - turn) * motorPowerScaling);
                 rightFront.setPower((-x + y - turn) * motorPowerScaling);
             }
-
             {
                 for (AprilTagDetection detection : vision.getDetections()) {
                     telemetry.addData("AprilTag ID", detection.id);
